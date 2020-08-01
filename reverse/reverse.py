508cc58c3db8cf35b5f72d2a81e4d4bda5384be9
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-08-01 01:12:48 -0700

    reverse completed, tests pass

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -16,6 +16,16 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def __str__(self):
+        elements = []
+
+        element = self.head
+        while element is not None:
+            elements.append(element.get_value())
+            element = element.get_next()
+
+        return str(elements)
+
     def add_to_head(self, value):
         node = Node(value)
 
@@ -38,5 +48,31 @@ class LinkedList:
 
         return False
 
-    def reverse_list(self, node, prev):
-        pass
+    def reverse_list(self, node, prev=None):
+        if node is None:
+            return None
+        if node.get_next() is None:
+            self.head = node
+            return
+        if node.get_next() is not None:
+            print('while', node.value, 'next ->', node.next_node.value, "head ->", self.head.value)
+            self.reverse_list(node.get_next())
+            old_after = node.get_next()
+            old_after.set_next(node)
+            node.set_next(None)
+            
+        
+
+ll = LinkedList()
+ll.add_to_head(1)
+ll.add_to_head(2)
+ll.add_to_head(3)
+ll.add_to_head(4)
+ll.add_to_head(5)
+print(ll)
+print(ll.head.value, ' == 5?')
+ll.reverse_list(ll.head)
+print(ll)
+print(ll.head.value, ' == 1?')
+print(ll.head.get_next().value, ' == 2?')
+print(ll.head.get_next().get_next().value,' == 3?')
